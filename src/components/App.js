ea10a4a599d29834e945a15a9fc5f6a274966983
sabrina-code <54184701+sabrina-code@users.noreply.github.com>
2020-04-12 13:29:30 -0700

    initial form

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
     render() {
         return (
-            <SearchBar />
+            <div className="" >
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
         );
     }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import './style.css';
 
 class SearchBar extends React.Component {
     state = { term: '' };
 
     onFormSubmit = event => {
         event.preventDefault();
-        console.log(this.state.term);
+        // console.log(this.state.term);
+        this.props.onSubmit(this.state.term); //passing down props.onSubmit from App
     };
 
     render() {
         return (
-            <div className="searcharea">
+            <div className="searcharea container">
                 <form onSubmit={this.onFormSubmit} className="searchForm">
                     <label>Book search</label>
                     <input
@@ -18,6 +20,7 @@ class SearchBar extends React.Component {
                         value={this.state.term}
                         onChange={e => this.setState({ term: e.target.value })}
                     />
+                    <button type="submit" >Search</button>
                 </form>
             </div>
         )
