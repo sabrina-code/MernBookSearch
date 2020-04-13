fe07a2232befe644301aee7ae1127e8cd0c21ddb
sabrina-code <54184701+sabrina-code@users.noreply.github.com>
2020-04-13 09:32:33 -0700

    booklist placement

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const BookList = props => {
-    const books = props.books.map((book) => {
-        return <img alt={book.volumeInfo.title} src={book.volumeInfo.imageLinks.thumbnail} />
+    const books = props.books.map(({ id, volumeInfo }) => {
+        return <img key={id} alt={volumeInfo.title} src={volumeInfo.imageLinks.thumbnail} />
     });
-    return <div>{books}</div>;
+    return (
+
+        <div className="booklist">{books}</div>
+
+    )
 };
 
 export default BookList;
+
+/* pre destructure:
+const books = props.books.map((book) => {
+    return <img key={book.id} alt={book.volumeInfo.title} src={book.volumeInfo.imageLinks.thumbnail} />
+}); */
